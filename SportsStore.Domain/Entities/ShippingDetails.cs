3aa02f2ca36315d392ecc030731b9d3c253bd28e
Sam Amara <amaramas@my.erau.edu>
2019-05-23 14:05:45 -0400

    improved shipping view using ViewData.ModelMetaData

--- SportsStore.Domain/Entities/ShippingDetails.cs
+++ SportsStore.Domain/Entities/ShippingDetails.cs
@@ -9,8 +9,11 @@ namespace SportsStore.Domain.Entities
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Please enter the first address line")]
+        [Display(Name = "Line 1")]
         public string Line1 { get; set; }
+        [Display(Name = "Line 2")]
         public string Line2 { get; set; }
+        [Display(Name = "Line 3")]
         public string Line3 { get; set; }
 
         [Required(ErrorMessage = "Please enter a city name")]
