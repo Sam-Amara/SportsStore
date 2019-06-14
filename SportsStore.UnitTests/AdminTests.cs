e9902735f82e580b0b22fd1b996b38c4d2a3be38
Sam Amara <amaramas@my.erau.edu>
2019-06-14 19:51:20 -0400

    added unit test for index method in Admin controller

---
+++ SportsStore.UnitTests/AdminTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+
+        [TestMethod]
+        public void Index_Contains_All_Products()
+        {
+            // Arrange - create the mock repository
+            var mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            });
+
+            // Arrange - create a controller
+            var target = new AdminController(mock.Object);
+
+            // Action
+            var result = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual("P1", result[0].Name);
+            Assert.AreEqual("P2", result[1].Name);
+            Assert.AreEqual("P3", result[2].Name);
+        }
+    }
+}
